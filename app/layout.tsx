d8880c7f81fd90e21281c0ac3599a9306c3909e7
YUKITAKA YAMASHITA <takasnowstyle1821@gmail.com>
2025-08-13 02:01:58 +0900

    ユーザーのログイン状態を確認して、プロフィール情報を自動で更新する仕組みを追加し、 全ページでこの仕組みを使うようにした

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import SupabaseListener from './components/supabase-listener'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,18 +16,21 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <body className={inter.className}>
-      <div className="flex flex-col min-h-screen">
+    <html>
+      <body className={inter.className}>
+        <div className="flex flex-col min-h-screen">
 
+          {/* @ts-expect-error next version of TS will fix this */}
+          <SupabaseListener />
+          <main className="flex-1 container max-w-screen-sm mx-auto px-1 py-5">{children}</main>
 
-        <main className="flex-1 container max-w-screen-sm mx-auto px-1 py-5">{children}</main>
-
-        <footer className="py-5">
-          <div className="text-center text-sm">
-            Copyright © All rights reserved | FullStackChannel
-          </div>
-        </footer>
-      </div>
-    </body>
+          <footer className="py-5">
+            <div className="text-center text-sm">
+              Copyright © All rights reserved | FullStackChannel
+            </div>
+          </footer>
+        </div>
+      </body>
+    </html>
   )
 }
