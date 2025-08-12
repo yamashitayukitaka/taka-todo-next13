f070daaeeb46f35611137ac54149b92d319acf21
YUKITAKA YAMASHITA <takasnowstyle1821@gmail.com>
2025-08-13 01:55:51 +0900

    feat: サーバーコンポーネントでSupabase認証セッションを取得し、ログイン状態を表示する

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+// App Router（app/ディレクトリ）のReactコンポーネントは
+// 基本的にデフォルトでサーバーコンポーネント（'use server'相当）
+// Supabaseの認証情報を安全に取得し、その結果に応じてUIを切り替えるため
+// このコンポーネントはサーバーコンポーネントとして実装する必要がある
 
-const Home = () => {
+
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
+
+import type { Database } from '@/lib/database.types'
+
+// メインページ
+const Home = async () => {
+  const supabase = createServerComponentClient<Database>({
+    cookies,
+  })
+
+  // セッションの取得
+  const {
+    data: { session },
+  } = await supabase.auth.getSession()
+
   return (
-    <div>
-      <div>Main</div>
+    <div className="text-center text-xl">
+      {session ? <div>ログイン済</div> : <div>未ログイン</div>}
     </div>
   )
 }
