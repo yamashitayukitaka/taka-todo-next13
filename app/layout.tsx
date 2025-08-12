c64532e0e2e628d266d29fda0c21e61acf9f1735
YUKITAKA YAMASHITA <takasnowstyle1821@gmail.com>
2025-08-13 02:17:51 +0900

    fix: SupabaseListenerコンポーネントのimportパスを '@/app/components/supabase-listener' に修正

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
-import SupabaseListener from './components/supabase-listener'
+import SupabaseListener from '@/app/components/supabase-listener'
 
 const inter = Inter({ subsets: ['latin'] })
 
