6a0d016d88367b91f41c09dac0fca86e5c5ee7e0
YUKITAKA YAMASHITA <takasnowstyle1821@gmail.com>
2025-08-14 01:51:43 +0900

    feat: BBSCardListコンポーネントを作成し、BBSData配列を受け取ってBBSCardをグリッド表示
    propsで渡されたbbsAllDataをmapで展開
    各BBSCardにReactのkeyを付与し識別
    子コンポーネントにkeyを渡さず親で管理する仕様を明示

---
+++ app/components/layouts/BBSCardList.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { BBSData } from "../types/types";
+import BBSCard from '@/app/components/layouts/BBSCard';
+
+interface BBSAllDataProps {
+  bbsAllData: BBSData[];
+}
+
+// props で受け取るオブジェクトの構造を明示するため、キー名（bbsAllData）を含めた型定義が必要
+const BBSCardList = ({ bbsAllData }: BBSAllDataProps) => {
+  return (
+    <div className="grid lg:grid-cols-3 px-4 py-4 gap-4">
+      {bbsAllData.map((bbsData: BBSData) => (
+        <BBSCard
+          key={bbsData.id}// ← React専用の識別子
+
+          // key は親コンポーネントで、map関数のように繰り返し処理する 
+          // JSX 要素に直接指定する必要がある。子コンポーネントに分離しても、
+          // key は子コンポーネント内で JSX に記述しても React には認識されない。
+          // key は props として渡すこともできない（React により内部で処理され、子には渡らない）
+          // なので子コンポネントに分離する場合は親コンポネント内の
+          // 繰り返し処理の中で<子コンポネント名　key = {}/>
+          // とかく必要がある
+
+          bbsData={bbsData} // ← これはprops
+        />
+      ))}
+    </div>
+  )
+}
+
+
+export default BBSCardList
+
+
