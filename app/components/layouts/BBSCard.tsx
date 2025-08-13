c8f7639340bb9cea86904031c389f7e11b719f7e
YUKITAKA YAMASHITA <takasnowstyle1821@gmail.com>
2025-08-14 01:52:38 +0900

    feat: BBSCardコンポーネントを作成し、BBSDataのタイトル・ユーザー名・内容をカード形式で表示
    CardHeaderにタイトルとユーザー名を表示
    CardContentに投稿内容を表示
    CardFooterに投稿詳細ページへのリンクを追加

---
+++ app/components/layouts/BBSCard.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import Link from 'next/link';
+
+
+import { BBSData } from "../types/types";
+import {
+  Card,
+  // CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+
+
+interface BBSDataProps {
+  bbsData: BBSData;
+}
+
+const BBSCard = ({ bbsData }: BBSDataProps) => {
+  const { id, username, title, content } = bbsData;
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>{title}</CardTitle>
+        <CardDescription>{username}</CardDescription>
+        {/* <CardAction>
+        </CardAction> */}
+      </CardHeader>
+      <CardContent>{content}</CardContent>
+      <CardFooter className="flex-col gap-2">
+        <Link href={`/bbs-posts/${id}`} className="text-blue-500">Read More</Link>
+        {/* このように バッククオート（``）で囲んだテンプレートリテラルはJavaScriptの式なので、
+        JSX内では中括弧 {} で囲んで式として渡す必要があります。 */}
+      </CardFooter>
+    </Card>
+  )
+}
+
+export default BBSCard
