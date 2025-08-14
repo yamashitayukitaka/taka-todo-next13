6be09e95a36fa0fdfae48d62832ff74dc6cd7608
YUKITAKA YAMASHITA <takasnowstyle1821@gmail.com>
2025-08-14 10:29:29 +0900

    fix:パスを修正

--- app/components/layouts/BBSCard.tsx
+++ app/components/layouts/BBSCard.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Link from 'next/link';
 
 
-import { BBSData } from "../types/types";
+import { BBSData } from "@/app/types/types";
 import {
   Card,
   // CardAction,
